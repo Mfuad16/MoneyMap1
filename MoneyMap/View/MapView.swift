0896facfb2078429d301c0111d9e68398d5aff82
Mfuad16 <fuadmoha16@gmail.com>
2024-08-31 12:22:27 +0900

    added MapView

---
+++ MoneyMap/View/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  MoneyMap
+//
+//  Created by Mohamed Fuad on 8/30/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
